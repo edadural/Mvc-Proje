3334f54aec4b729e1c799f0e4a0d246a78c78610
Eda Dural <edaadurall@gmail.com>
2023-02-19 23:22:51 +0300

    22-41

---
+++ MvcProjeKampi/Controllers/AdminCategoryController.cs
@@ -0,0 +1,72 @@
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccsessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class AdminCategoryController : Controller
+    {
+        CategoryManager cm = new CategoryManager(new EfCategoryDal());
+        public ActionResult Index()     // veri listelencek
+        {
+            var categoryValues = cm.GetList();
+            return View(categoryValues);
+        }
+
+        [HttpGet]
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddCategory(Category p)
+        {
+            CategoryValidator categoryValidator = new CategoryValidator();
+            ValidationResult results = categoryValidator.Validate(p);
+
+            if (results.IsValid)
+            {
+                cm.CategoryAdd(p);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+
+        public ActionResult DeleteCategory(int id)
+        {
+            var categoryvalue = cm.GetByID(id);     // parametre olarak aldigin id degerine gore ilgili satiri getir
+            cm.CategoryDelete(categoryvalue);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult EditCategory(int id)
+        {
+            var categoryvalue = cm.GetByID(id);
+            return View(categoryvalue);         // degisken icerigiyle dondur
+        }
+
+        [HttpPost]
+        public ActionResult EditCategory(Category p)
+        {
+            cm.CategoryUpdate(p);
+            return RedirectToAction("Index");
+        }
+
+    }
+}
